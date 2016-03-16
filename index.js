9e275cef9d04f67738781fe70c440beaf6bcb6d7
Mejdi Ourir <m.ourir@ivsweb.com>
2016-03-16 12:23:10 +0100

    remove Promise

--- index.js
+++ index.js
@@ -2,24 +2,34 @@ const exec = require('child_process').exec;
 
 const HDMI_ON_CMD       = "/opt/vc/bin/tvservice -p"
 const HDMI_OFF_CMD      = "/opt/vc/bin/tvservice -o"
-const HDMI_STATUS_CMD   = "/opt/vc/bin/tvservice -o"
+const HDMI_STATUS_CMD   = "/opt/vc/bin/tvservice --status"
 
 
-
-module.exports.status = function(){
-  return new Promise(function(resolve, reject){
-    exec(HDMI_STATUS_CMD , function(error, stdout){
-      if(error !== null)
-        reject(error);
-      resolve(stdout);
-    })
+var runCmd = function(cmd, chain){
+  exec(cmd , function(error, stdout){
+    if(error !== null)
+      return chain(error);
+    var status = "";
+    stdout.on("data" , function(data){
+      status += data;
+    });
+    stdout.on("end" , function(){
+      chain(null , status);
+    });
+    stdout.on("error" , function(err){
+      chain(err);
+    });
   })
 }
 
-module.exports.on = function(){
-  return exec(HDMI_ON_CMD);
+module.exports.status = function(chain){
+  runCmd(HDMI_STATUS_CMD , chain);
 }
 
-module.exports.off = function(){
-  return exec(HDMI_OFF_CMD);
+module.exports.on = function(chain){
+  runCmd(HDMI_ON_CMD , chain);
+}
+
+module.exports.off = function(chain){
+  runCmd(HDMI_OFF_CMD , chain);
 }
